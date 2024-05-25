d288f414e56c00709d42385236daa4678fa5715a
eslam-obeida <eslamobeidat71@gmail.com>
2024-05-25 06:02:09 -0700

    update

---
+++ my-react-app/src/newParagraph.js
@@ -0,0 +1,14 @@
+
+import logo from './logo.svg';
+import './App.css';
+
+function Paragraph() {
+  return (
+    <div>
+      <h1>NEW Hello React JS</h1>
+    </div>
+  );
+}
+
+
+export default Paragraph;
